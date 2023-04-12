7959576f280b0ef14d0c8a378ca7a7f4cbe3cde9
Alex <114597291+Qa1ro@users.noreply.github.com>
2023-04-12 21:46:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
   control_proportion = x_success / x_cnt
   test_proportion = y_success / y_cnt
   p_pool = (x_success + y_success) / (x_cnt + y_cnt)
-  z_score = (test_proportion - control_proportion) / (p_pool * (1 - p_pool)**(0.5) * (1/x_cnt + 1/y_cnt))
+  z_score = (test_proportion - control_proportion) / ((p_pool * (1 - p_pool) * (1/x_cnt + 1/y_cnt))**(0.5))
   if z_score > 1.96 or z_score < -1.96:
       return 1
   else:
