c6a51269c0d3ed2b1865c017fd1d35558f047ac8
Alex <114597291+Qa1ro@users.noreply.github.com>
2023-04-12 21:40:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 736941004 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+  control_proportion = x_success / x_cnt
+  test_proportion = y_success / y_cnt
+  p_pool = (x_success + y_success) / (x_cnt + y_cnt)
+  z_score = (test_proportion - control_proportion) / (p_pool * (1 - p_pool)**(0.5) * (1/x_cnt + 1/y_cnt))
+  if z_score > 1.96 or z_score < -1.96:
+      return 1
+  else:
+      return 0
